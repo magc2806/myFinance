b10f7cb2f47d319e39bbb59dfcc240c3691bbc82
magc2806 <gonzalezcabellomanuel@gmail.com>
2022-07-10 20:38:53 -0400

    index, new and create bank_accounts actions are ready. Most bank_accounts views are with internationalization. General no reuslt message added

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -1,15 +1,58 @@
 class BankAccountsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_bank_account, only: %i[edit update destroy ]
+  before_action :set_bank_account, only: %i[create]
 
 
   def index
+    @bank_accounts = current_user.bank_accounts
     
   end
 
+  def new
+    @bank_account = current_user.bank_accounts.new    
+  end
+
+  def create
+
+    respond_to do |format|
+      if @bank_account.save
+        format.html { redirect_to bank_accounts_path, notice: I18n.t('views.common.messages.successful_creation')}
+        format.json { render bank_accounts_path, status: :created, location: @bank_account }        
+      else
+        flash[:alert] = @bank_account.errors.full_messages
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @bank_account.errors, status: :unprocessable_entity }
+      end    
+    end
+
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
   def find_bank_account
-    @bank_account = BankAccount.find(params[:id])
-        
+    @bank_account = current_user.bank_accounts.find(params[:id])
+
+  end
+
+  def set_bank_account
+    @bank_account = current_user.bank_accounts.new(bank_account_params)
+
+  end
+
+  def bank_account_params
+
+    params.require(:bank_account).permit(:name,:account_number, :bank_name)
+    
   end
 
 end
