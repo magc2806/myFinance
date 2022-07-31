fe0a1cfbecf1d6c76ea97948d290a0076ece1f94
magc2806 <gonzalezcabellomanuel@gmail.com>
2022-07-31 09:50:19 -0400

    updating transaction from job

--- app/jobs/hello_world_job.rb
+++ app/jobs/hello_world_job.rb
@@ -1,7 +1,9 @@
 class HelloWorldJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(transaction_id)
+    transaction = Transaction.find transaction_id
+    transaction.update(description: "Actualizado desde Job")
     # Simulates a long, time-consuming task
     sleep 5
     # Will display current time, milliseconds included
