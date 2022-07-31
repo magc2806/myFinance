199171b8dbf0df2ecdf44d984558f93006b70a24
magc2806 <gonzalezcabellomanuel@gmail.com>
2022-07-30 20:39:07 -0400

    trying to send emails in production2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,8 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
   #mailgun settings
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
